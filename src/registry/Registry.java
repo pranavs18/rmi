25a781e3297fb3b430115757e54df3e0cf5c4f80
Pranav Saxena <psbits@gmail.com>
2014-06-15 01:43:14 -0400

    registry server socket implementation

--- src/registry/Registry.java
+++ src/registry/Registry.java
@@ -1,26 +1,34 @@
 package registry;
 import generics.myRemoteInterface;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Registry implements RegistryInterface, Runnable {
 	
-	
-	private String host;
+	public final static int registry_port = 1099;
+	private static String host;
 	private int port;
-	ConcurrentHashMap<String, RemoteObjectRef> regMap = new ConcurrentHashMap<String,RemoteObjectRef>();
-	
+	ConcurrentHashMap<String, RemoteObjectRef> regMap;
+
 	//Constructor to instantiate rmiRegistry 
 	public Registry(String ipAddress, int port){
 		this.setHost(ipAddress);
 		this.setPort(port);
+		regMap = new ConcurrentHashMap<String, RemoteObjectRef>();
 	}
 	
 	public void bind(String name, RemoteObjectRef obj) throws  AlreadyBoundException, RemoteException{
 		if(!regMap.containsKey(name)){
-			regMap.put(name, obj);
+		//	RemoteObjectRef ROR = new RemoteObjectRef(host,port);
+			//regMap.put(name, );
 			
 		}
 		else{
@@ -52,22 +60,48 @@ public class Registry implements RegistryInterface, Runnable {
 		this.host = host;
 	}
 
+	
 	@Override
 	public void run() {
-		while (true) {
                 
-                 System.out.println("Accept incoming connections");
-                 try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
+			try {
+				try {
+					startRegistry(host, registry_port);
+				} catch (ClassNotFoundException e) {
 					e.printStackTrace();
 				}
-               
-
-              
-        
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+   
+	}
+	
+private void startRegistry(String host, int registryPort) throws IOException, ClassNotFoundException {
 		
-	           }
+	@SuppressWarnings("resource")
+	ServerSocket registryServer = new ServerSocket(registryPort);
+	while(true){
+		
+		Socket registrySocket = registryServer.accept();	
+		ObjectInputStream regSocketInput = new ObjectInputStream(registrySocket.getInputStream());
+		regSocketInput.readObject();
+		
+	}
+}
+
+public static void main(String[] args){
+	
+	    	
+		if(args.length != 1){
+			System.out.println("Please enter the Arguments of the form - Registry_IP");
+			
+		}
+		
+		String Registry_IP = args[0]; 
+		host = Registry_IP;
+		Registry reg = new Registry(Registry_IP,registry_port);	
+		System.out.println("Starting registry on host " + Registry_IP + " and port " + registry_port);
+		new Thread(reg).start();
 	}
 	
 }
